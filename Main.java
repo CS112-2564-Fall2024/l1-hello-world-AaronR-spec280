8d8857360247edc7c5724212c9bbbc6ead3f4a4f
AaronR-spec280 <AaronPRZ04@gmail.com>
2024-08-20 23:08:02 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS 112!");
+        noReturn;
+        introduction("Aaron", 18);
+        introduction("Victor", 22);
+        introcution("Samuel", 34);
     }
+    
+//First method
+    static void noReturn(){
+        System.out.println("Return text");
+            }
+
+//Second method
+    static void introduction(String name, int age){
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+    
 }
